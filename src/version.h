7404bfc2012fa7c5e8b97f18d6e791e95ac1f099
David Lindauer <touchstone222@runbox.com>
2015-04-12 00:30:24 -0400

    released

--- src/version.h
+++ src/version.h
@@ -1,11 +1,8 @@
-#define STRING_VERSION "5.32.0.1"
-#define PRODUCT_VERSION 5,32,0,1
-#define CLIB_VERSION 5,32,0,1
+#define STRING_VERSION "5.33.0.1"
+#define PRODUCT_VERSION 5,33,0,1
+#define CLIB_VERSION 5,33,0,1
 #define PRODUCT_STRING_VERSION STRING_VERSION
 #define PRODUCT_NAME "Orange C Compiler Package"
 #define CLIB_STRING_VERSION STRING_VERSION
 #define COPYRIGHT "Copyright (C) LADSoft 2006-2015"
 #define LIBRARY_COPYRIGHT "Copyright (C) LADSoft 1994-2015"
-#define IDS_Pears_are_sweet 1001
-#define IDS_Peaches_are_cool 1000
-#define IDR_RCDATA_10104 10104

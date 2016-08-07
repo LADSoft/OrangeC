47041e6dfe1d958f5388a52bb9e6d9b14b6bcfd0
David Lindauer <touchstone222@runbox.com>
2016-08-07 04:55:12 -0400

    fix lockup

--- src/ocide/xedit.c
+++ src/ocide/xedit.c
@@ -2406,7 +2406,7 @@ LRESULT CALLBACK exeditProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM
                    {
                        if (p->cd)
                        {
-                            p->cd->colorizing++;
+//                            p->cd->colorizing++;
                             free(p->cd->lineData);
                             p->cd->lineData = NULL;
                             p->cd->lineDataMax = 0;
@@ -2416,7 +2416,7 @@ LRESULT CALLBACK exeditProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM
                                 DWINFO *x = (DWINFO *)GetWindowLong(t, 0);
                                 p->cd->lineData = ccGetLineData(x->dwName, &p->cd->lineDataMax);
                             }
-                            p->cd->colorizing--;
+//                            p->cd->colorizing--;
                        }
                         FreeColorizeEntries(p->colorizeEntries);
                         {

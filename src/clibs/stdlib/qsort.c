bccb0a0bde6cdb2f409943462248d2836bce481a
chuggafan <chuggafans@gmail.com>
2018-12-10 12:22:27 -0500

    Format qsort and make it more obvious what is going on

--- src/clibs/stdlib/qsort.c
+++ src/clibs/stdlib/qsort.c
@@ -1,57 +1,61 @@
 /* Software License Agreement
-* 
-*      Copyright(C) 1994-2018 David Lindauer, (LADSoft)
-* 
-*      This file is part of the Orange C Compiler package.
-* 
-*      The Orange C Compiler package is free software: you can redistribute it and/or modify
-*      it under the terms of the GNU General Public License as published by
-*      the Free Software Foundation, either version 3 of the License, or
-*      (at your option) any later version, with the addition of the
-*      Orange C "Target Code" exception.
-* 
-*      The Orange C Compiler package is distributed in the hope that it will be useful,
-*      but WITHOUT ANY WARRANTY; without even the implied warranty of
-*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*      GNU General Public License for more details.
-* 
-*      You should have received a copy of the GNU General Public License
-*      along with Orange C.  If not, see <http://www.gnu.org/licenses/>.
-* 
-*      contact information:
-*          email: TouchStone222@runbox.com <David Lindauer>
-* 
-*/
+ *
+ *      Copyright(C) 1994-2018 David Lindauer, (LADSoft)
+ *
+ *      This file is part of the Orange C Compiler package.
+ *
+ *      The Orange C Compiler package is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version, with the addition of the
+ *      Orange C "Target Code" exception.
+ *
+ *      The Orange C Compiler package is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with Orange C.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      contact information:
+ *          email: TouchStone222@runbox.com <David Lindauer>
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef int cmpfunc(const void*, const void*);
+#define SWAP(x, y)          \
+    unsigned char temp = x; \
+    x = y;                  \
+    y = temp;
 
 static void quicksort(unsigned char* data, size_t width, cmpfunc* func, size_t elems)
 {
-    int staticheap[4096*  4];
+    int staticheap[4096 * 4];
 
     int* heap = staticheap;
     if (elems >= 4096)
-        heap  = (int* )malloc(elems*sizeof(int)*  4+ 4 * sizeof(int));
+        heap = (int*)malloc(elems * sizeof(int) * 4 + 4 * sizeof(int));
     if (heap)
     {
         heap[0] = 0;
-        heap[1] = (elems-1)*  width;
+        heap[1] = (elems - 1) * width;
         size_t heapStart = 0;
         size_t heapSize = 2;
         while (heapStart < heapSize)
         {
-            size_t pivot=0;
+            size_t pivot = 0;
             if (heap[heapStart] < heap[heapStart + 1])
             {
                 unsigned low = heap[heapStart];
-                unsigned high = heap[heapStart+1];
+                unsigned high = heap[heapStart + 1];
                 // pivot (Element to be placed at right position)
                 unsigned char* pivotelem = data + high;
 
                 size_t pivot = low - width;
-                for (size_t j = low; j <= high-width; j += width)
+                for (size_t j = low; j <= high - width; j += width)
                 {
                     if (func(data + j, pivotelem) <= 0)
                     {
@@ -60,9 +64,7 @@ static void quicksort(unsigned char* data, size_t width, cmpfunc* func, size_t e
                         {
                             for (int k = 0; k < width; k++)
                             {
-                                unsigned char temp = data[pivot + k];
-                                data[pivot + k] = data[j + k];
-                                data[j + k] = temp;
+                                SWAP(data[pivot + k], data[j + k]);
                             }
                         }
                     }
@@ -70,9 +72,7 @@ static void quicksort(unsigned char* data, size_t width, cmpfunc* func, size_t e
                 pivot += width;
                 for (int k = 0; k < width; k++)
                 {
-                    unsigned char temp = data[pivot + k];
-                    data[pivot + k] = pivotelem[k];
-                    pivotelem[k] = temp;
+                    SWAP(data[pivot + k], pivotelem[k]);
                 }
                 heap[heapSize++] = heap[heapStart + 0];
                 heap[heapSize++] = pivot - width;
@@ -86,7 +86,4 @@ static void quicksort(unsigned char* data, size_t width, cmpfunc* func, size_t e
     }
 }
 
-void _RTL_FUNC qsort(void* base, size_t num, size_t width, int (*compare)(const void* elem1, const void* elem2))
-{
-    quicksort(base, width, compare, num);
-}
+void _RTL_FUNC qsort(void* base, size_t num, size_t width, cmpfunc compare) { quicksort(base, width, compare, num); }

e1525847b0e78efd30e488ccd7ed7d19014f0720
LADSoft <touchstone222@runbox.com>
2019-01-31 22:32:10 -0500

    add wait() and waitpid()

---
+++ src/clibs/stdinc/sys/wait.h
@@ -0,0 +1,83 @@
+/* Software License Agreement
+ * 
+ *     Copyright(C) 1994-2019 David Lindauer, (LADSoft)
+ * 
+ *     This file is part of the Orange C Compiler package.
+ * 
+ *     The Orange C Compiler package is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     The Orange C Compiler package is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with Orange C.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *     As a special exception, if other files instantiate templates or
+ *     use macros or inline functions from this file, or you compile
+ *     this file and link it with other works to produce a work based
+ *     on this file, this file does not by itself cause the resulting
+ *     work to be covered by the GNU General Public License. However
+ *     the source code for this file must still be made available in
+ *     accordance with section (3) of the GNU General Public License.
+ *     
+ *     This exception does not invalidate any other reasons why a work
+ *     based on this file might be covered by the GNU General Public
+ *     License.
+ * 
+ *     contact information:
+ *         email: TouchStone222@runbox.com <David Lindauer>
+ * 
+ */
+
+/*  timeb.h
+
+    Struct and function declarations for ftime().
+
+*/
+
+#if !defined(__WAIT_H)
+#define __WAIT_H
+
+#ifndef __DEFS_H__
+#include <_defs.h>
+#endif
+
+#ifndef __TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifdef __cplusplus
+namespace __STD_NS__ {
+extern "C" {
+#endif
+
+#define WNOHANG 1
+#define WUNTRACED 2
+
+#define WEXITSTATUS(x) (x & 0xff)
+#define WIFEXITED(x) (x >= 0)
+#define WIFSIGNALED(x) (0)
+#define WIFSTOPPED(x) (0)
+#define WSTOPSIG(x) (0)
+#define WIFCONTINUED(x) (0)
+
+pid_t _RTL_FUNC wait(int *);
+pid_t _RTL_FUNC waitpid(pid_t, int *, int);
+
+#ifdef __cplusplus
+} ;
+} ;
+#endif
+
+#pragma pack()
+#endif  /* __WAIT_H */
+#if defined(__cplusplus) && !defined(__USING_CNAMES__) && !defined(__WAIT_H_USING_LIST)
+using __STD_NS_QUALIFIER wait; 
+using __STD_NS_QUALIFIER waitpid;
+#endif
+

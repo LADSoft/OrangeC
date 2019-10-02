75c92d3d2021d425bf0c7d6d68402bdddb386101
David Lindauer <touchstone222@runbox.com>
2019-10-02 19:51:35 -0400

    #421 remove debug code

--- src/occ/middle/iconst.cpp
+++ src/occ/middle/iconst.cpp
@@ -1224,11 +1224,6 @@ static bool eval(QUAD* q)
     int rv = false;
     IMODE* val;
     enum vop set;
-    if (q->temps & TEMP_ANS)
-    {
-        if (q->ans->offset->v.sp->value.i == 544 || q->ans->offset->v.sp->value.i == 545)
-            printf("hi");
-    }
     if (q->dc.opcode == i_phi)
     {
         tnum = q->dc.v.phi->T0;

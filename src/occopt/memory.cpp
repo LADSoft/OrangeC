37fc85f583b36840a3068e9261417093ec4b4b19
David Lindauer <touchstone222@runbox.com>
2020-01-01 22:29:20 -0500

    #297 remove windows heap references to speed up compiles

--- src/occopt/memory.cpp
+++ src/occopt/memory.cpp
@@ -24,8 +24,7 @@
 
 #include "ctypes.h"
 #include "Utils.h"
-
-static MEMBLK* freestdmem;
+    
 static MEMBLK* globals;
 static MEMBLK* locals;
 /*static*/ MEMBLK* opts;
@@ -37,23 +36,12 @@ static MEMBLK* conflicts;
 
 
 
-static int globalFlag=1;
+static bool globalFlag=true;
 static int globalPeak, localPeak, optPeak, tempsPeak, aliasPeak, livePeak, templatePeak, conflictPeak;
 
 #define MINALLOC (128 * 1024)
 #define MALIGN (4)
 
-#ifdef _WIN32
-#    include <windows.h>
-static HANDLE globalHeap;
-static HANDLE localHeap;
-static HANDLE optHeap;
-static HANDLE aliasHeap;
-static HANDLE tempHeap;
-static HANDLE liveHeap;
-static HANDLE templateHeap;
-static HANDLE conflictHeap;
-#endif
 //#define DEBUG
 void mem_summary(void)
 {
@@ -68,59 +56,32 @@ void mem_summary(void)
     printf("\tConflict peak %dK\n", (conflictPeak + 1023) / 1024);
     globalPeak = localPeak = optPeak = tempsPeak = aliasPeak = livePeak = conflictPeak = 0;
 }
-static MEMBLK* galloc(MEMBLK** arena, HANDLE* heap, int size)
+static MEMBLK* galloc(MEMBLK** arena, int size)
 {
     MEMBLK* selected;
     int allocsize = size <= MINALLOC ? MINALLOC : (size + (MINALLOC - 1)) & -MINALLOC;
-    selected = nullptr;
-    if (allocsize == MINALLOC)
-    {
-        if (freestdmem)
-        {
-            selected = freestdmem;
-            freestdmem = freestdmem->next;
-        }
-    }
+    selected = (MEMBLK*)calloc(1, allocsize + sizeof(MEMBLK) - 1);
     if (!selected)
-    {
-#ifdef _WIN32
-        if (!*heap)
-            *heap = HeapCreate(HEAP_NO_SERIALIZE, 0, 0);
-        selected = (MEMBLK*)HeapAlloc(*heap, 0, allocsize + sizeof(MEMBLK) - 1);
-#else
-        selected = (MEMBLK*)malloc(allocsize + sizeof(MEMBLK) - 1);
-#endif
-        if (!selected)
-            Utils::fatal("out of memory");
-        selected->size = allocsize;
-    }
+        Utils::fatal("out of memory");
+    selected->size = allocsize;
     selected->left = selected->size;
-#ifdef DEBUG
-    memset(selected->m, 0xc4, selected->left);
-#else
-//    memset(selected->m, 0, selected->left);
-#endif
     selected->next = *arena;
     *arena = selected;
-    //    printf("%d %d\n", count1, count2);
     return selected;
 }
-void* memAlloc(MEMBLK** arena, HANDLE* heap, int size)
+void* memAlloc(MEMBLK** arena, int size)
 {
     MEMBLK* selected = *arena;
     void* rv;
     if (!selected || selected->left < size)
     {
-        selected = galloc(arena, heap, size);
+        selected = galloc(arena, size);
     }
     rv = (void*)(selected->m + selected->size - selected->left);
-    //#ifdef DEBUG
-    memset(rv, 0, size);
-    //#endif
     selected->left = selected->left - ((size + MALIGN - 1) & -MALIGN);
     return rv;
 }
-void memFree(MEMBLK** arena, HANDLE* heap, int* peak)
+void memFree(MEMBLK** arena, int* peak)
 {
     MEMBLK* freefind = *arena;
     long size = 0;
@@ -129,61 +90,43 @@ void memFree(MEMBLK** arena, HANDLE* heap, int* peak)
     while (freefind)
     {
         MEMBLK* next = freefind->next;
-#ifdef DEBUG
-        memset(freefind->m, 0xc4, freefind->size);
-#endif
         size += freefind->size;
-#ifndef _WIN32
-        if (freefind->size == MINALLOC)
-        {
-            freefind->next = freestdmem;
-            freestdmem = freefind;
-        }
-        else
-        {
-            free(freefind);
-        }
-#endif
+        free(freefind);
         freefind = next;
     }
-#ifdef _WIN32
-    HeapDestroy(*heap);
-    *heap = nullptr;
-#endif
     *arena = 0;
     if (size > *peak)
         *peak = size;
 }
-void* globalAlloc(int size) { return memAlloc(&globals, &globalHeap, size); }
+void* globalAlloc(int size) { return memAlloc(&globals, size); }
 void globalFree(void)
 {
-    memFree(&globals, &globalHeap, &globalPeak);
+    memFree(&globals, &globalPeak);
     globalFlag = 1;
 }
-void* localAlloc(int size) { return memAlloc(&locals, &localHeap, size); }
-void localFree(void) { memFree(&locals, &localHeap, &localPeak); }
+void* localAlloc(int size) { return memAlloc(&locals, size); }
+void localFree(void) { memFree(&locals, &localPeak); }
 void* Alloc(int size)
 {
 #ifndef PARSER_ONLY
     if (!globalFlag)
-        return memAlloc(&locals, &localHeap, size);
+        return memAlloc(&locals, size);
 #endif
-    return memAlloc(&globals, &globalHeap, size);
+    return memAlloc(&globals, size);
 }
-void* oAlloc(int size) { return memAlloc(&opts, &optHeap, size); }
-void oFree(void) { memFree(&opts, &optHeap, &optPeak); }
-void* aAlloc(int size) { return memAlloc(&alias, &aliasHeap, size); }
-void aFree(void) { memFree(&alias, &aliasHeap, &aliasPeak); }
-void* tAlloc(int size) { return memAlloc(&temps, &tempHeap, size); }
-void tFree(void) { memFree(&temps, &tempHeap, &tempsPeak); }
-void* cAlloc(int size) { return memAlloc(&conflicts, &conflictHeap, size); }
-void cFree(void) { memFree(&conflicts, &conflictHeap, &conflictPeak); }
-void* sAlloc(int size) { return memAlloc(&live, &liveHeap, size); }
-void sFree(void) { memFree(&live, &liveHeap, &livePeak); }
-void IncGlobalFlag(void) { globalFlag++; }
-void DecGlobalFlag(void) { globalFlag--; }
-void SetGlobalFlag(int flag) { globalFlag = flag; }
-int GetGlobalFlag(void) { return globalFlag; }
+void* oAlloc(int size) { return memAlloc(&opts, size); }
+void oFree(void) { memFree(&opts, &optPeak); }
+void* aAlloc(int size) { return memAlloc(&alias, size); }
+void aFree(void) { memFree(&alias, &aliasPeak); }
+void* tAlloc(int size) { return memAlloc(&temps, size); }
+void tFree(void) { memFree(&temps, &tempsPeak); }
+void* cAlloc(int size) { return memAlloc(&conflicts, size); }
+void cFree(void) { memFree(&conflicts, &conflictPeak); }
+void* sAlloc(int size) { return memAlloc(&live, size); }
+void sFree(void) { memFree(&live, &livePeak); }
+void SetGlobalFlag(bool flag, bool &old) { old = globalFlag, globalFlag = flag; }
+void ReleaseGlobalFlag(bool old) { globalFlag = old;  }
+bool GetGlobalFlag(void) { return globalFlag; }
 char* litlate(const char* name)
 {
     int l;
@@ -198,9 +141,7 @@ LCHAR* wlitlate(const LCHAR* name)
     LCHAR* rv;
     while (*p)
         p++, count++;
-    IncGlobalFlag();
     rv = (LCHAR*)Alloc((count + 1) * sizeof(LCHAR));
-    DecGlobalFlag();
     p = name;
     count = 0;
     while (*p)

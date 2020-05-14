343159d49a7b9b9e612bd83ab83b53a2b9e9f4c9
David Lindauer <touchstone222@runbox.com>
2020-05-14 19:43:18 -0400

    exploratory change

--- src/occopt/ilazy.cpp
+++ src/occopt/ilazy.cpp
@@ -968,7 +968,7 @@ static void HandleRO(QUAD* after, int tn)
             }
             after = after->back;
 
-        } while (!after->OCP && after->dc.opcode != i_label && after->dc.opcode != i_tag);
+        } while (!after->OCP && after->dc.opcode != i_label);
 }
 static void MoveExpressions(void)
 {

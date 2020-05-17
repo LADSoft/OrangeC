09b40c55729d80aed95a7fe4a6c78f4fcd8d247a
David Lindauer <touchstone222@runbox.com>
2020-05-17 19:12:07 -0400

    #507 fix warnings related to std::runtime

--- src/oimplib/ImpLibMain.cpp
+++ src/oimplib/ImpLibMain.cpp
@@ -48,10 +48,6 @@ int main(int argc, char** argv)
     {
         std::cout << e.what() << std::endl;
     }
-    catch (std::ios_base::failure)
-    {
-        Utils::fatal("Fatal Error...");
-    }
     return 1;
 }
 
--- src/onm/nmMain.cpp
+++ src/onm/nmMain.cpp
@@ -83,10 +83,6 @@ int main(int argc, char** argv)
     {
        std::cout << e.what() << std::endl;
     }
-    catch (std::ios_base::failure)
-    {
-        Utils::fatal("Fatal Error...");
-    }
     return 1;
 }
 int nmMain::Run(int argc, char** argv)

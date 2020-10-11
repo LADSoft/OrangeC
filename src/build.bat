19c5ce04601fb4270a3b699cfd43309b571b9e52
David Lindauer <touchstone222@runbox.com>
2020-10-11 19:58:10 -0400

    #257 instrument

--- src/build.bat
+++ src/build.bat
@@ -1,8 +1,10 @@
 @echo off
+echo path=%PATH%
      if (%TRAVIS_OS_NAME% NEQ "") (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 	 PATH=%ORANGEC%;%PATH%;c:\Program Files (x86)\Inno Setup 6
      )
+echo path=%PATH%
               cd c:\orangec\src
               echo WScript.Echo(Math.floor(new Date().getTime()/1000)); > %temp%\time.js
               for /f %%i in ('cscript //nologo %temp%\time.js') do set SOURCE_DATE_EPOCH=%%i

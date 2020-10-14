60754510df68e878b611c68160df507889150b31
David Lindauer <touchstone222@runbox.com>
2020-10-13 20:12:24 -0400

    latest version of config.bat

--- src/config.bat
+++ src/config.bat
@@ -1,3 +1,3 @@
 set ORANGEC=\orangec
-set PATH=\orangec\bin;\mingw64\mingw64\bin;%PATH%;\program files\llvm\bin
+set PATH=\orangec\bin;\mingw64\w64\bin;%PATH%;\program files\llvm\bin
 call vcvars32.bat

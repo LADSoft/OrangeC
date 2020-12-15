958e70e7977047984a7765119c036cd8ad21008e
David Lindauer <touchstone222@runbox.com>
2020-12-15 17:35:45 -0500

    add bad_alloc.cpp back in

---
+++ src/clibs/cpp/libcxx/src/bad_alloc.cpp
@@ -0,0 +1,48 @@
+//===---------------------- bad_alloc.cpp ------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "__config"
+#include "new"
+
+namespace std {
+
+bad_alloc::bad_alloc()
+{
+}
+
+bad_alloc::~bad_alloc() _NOEXCEPT
+{
+}
+
+const char *bad_alloc::what() const _NOEXCEPT
+{
+    return "bad_alloc";
+}
+void
+__throw_bad_alloc()
+{
+#ifndef _LIBCPP_NO_EXCEPTIONS
+    throw bad_alloc();
+#endif
+}
+
+bad_array_new_length::bad_array_new_length() : bad_alloc()
+{
+}
+
+bad_array_new_length::~bad_array_new_length() _NOEXCEPT
+{
+}
+
+const char *bad_array_new_length::what() const _NOEXCEPT
+{
+    return "bad_array_new_length";
+}
+
+}

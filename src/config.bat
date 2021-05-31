3932a333b408a3300736ad1aa10721407450e2ab
LADSsoft <touchstone222@runbox.com>
2021-05-30 22:07:48 -0400

    add valx to path

--- src/config.bat
+++ src/config.bat
@@ -1,4 +1,4 @@
 set ORANGEC=\orangec
 set ORANGEC_HOME=orangec
-set PATH=\orangec\bin;\mingw64\w64\bin;%PATH%;\program files\llvm\bin
+set PATH=\orangec\bin;\mingw64\w64\bin;%PATH%;\program files\llvm\bin;\tools\bin\win32
 call vcvars32.bat

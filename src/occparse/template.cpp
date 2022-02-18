07f45ca6e7b9f9386d362cb9be301f4f3ae43ee3
LADSsoft <touchstone222@runbox.com>
2022-02-18 13:25:48 -0500

    fix so algorithms tests will complete again

--- src/occparse/template.cpp
+++ src/occparse/template.cpp
@@ -7122,6 +7122,7 @@ void TemplatePartialOrdering(SYMBOL** table, int count, FUNCTIONCALL* funcparams
 }
 static bool comparePointerTypes(TYPE* tpo, TYPE* tps)
 {
+    TYPE* lastIndirect = nullptr;
     while (tpo && tps)
     {
         tpo = basetype(tpo);
@@ -7129,7 +7130,12 @@ static bool comparePointerTypes(TYPE* tpo, TYPE* tps)
         if (!tpo || !tps)
             return false;
         if (tpo->type == bt_templateparam)
+        {
+            if (tpo == lastIndirect)
+                return true;
+            lastIndirect = tpo;
             tpo = tpo->templateParam->p->byClass.dflt;
+        }
         if (tps->type == bt_templateparam)
             tps = tps->templateParam->p->byClass.dflt;
         if (!tpo || !tps || tpo->type == bt_templateparam || tps->type == bt_templateparam)

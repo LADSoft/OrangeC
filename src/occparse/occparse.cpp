5fd417bb0e8e9e1931856bf120cd2379d4d11da0
LADSoft <touchstone222@runbox.com>
2022-12-10 02:00:55 -0500

    replace operator new, operator delete with a caching version

--- src/occparse/occparse.cpp
+++ src/occparse/occparse.cpp
@@ -69,6 +69,48 @@
 #include "constexpr.h"
 #include "symtab.h"
 #include "ListFactory.h"
+
+#include <cstdlib>
+#include <cstdio>
+void *dictionary[128];
+
+void *operator new(unsigned aa)
+{
+    if (!aa)
+        return nullptr;
+
+    if (aa + 3 < 128 * 4)
+    {
+        int bb = aa + 3;
+        if (dictionary[bb/4])
+        {
+            void *rv = dictionary[bb/4];
+            dictionary[bb/4] = *(void **)((char *)rv+4);
+            return (void *)((char *)rv + 8);
+        }
+    }
+    aa += 11;
+    aa = aa * 8 / 8;
+    void *rv = malloc(aa);
+    *(unsigned *)rv = aa - 8;
+    return (void *)((char *)rv + 8);
+}
+void operator delete(void *p)
+{
+    if (!p)
+        return;
+    int n = *(unsigned *)((char *)p-8);
+    if (n < 128 * 4)
+    {
+        *(void **)((char *)p-4) = dictionary[n/4];
+        dictionary[n/4] = (void *)((char *)p-8);
+    }
+    else
+    {
+        free((char *)p-8);
+    }
+}
+
 using namespace DotNetPELib;
 PELib* peLib;
 

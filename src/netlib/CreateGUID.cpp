022fe58d1a72fde5059d3b9e7c7e1364cacf2044
LADSoft <touchstone222@runbox.com>
2023-07-06 22:46:37 -0400

    allow CreateGUID.cpp to compile on linux

--- src/netlib/CreateGUID.cpp
+++ src/netlib/CreateGUID.cpp
@@ -39,9 +39,8 @@ void DotNetPELib::PEWriter::CreateGuid(Byte* Guid)
     // that shouldn't be a problem on OS we are interested in.
     std::random_device dev;
     std::mt19937 engine(dev());
-    auto generator = std::bind(distribution, engine);
-
-    std::generate(rnd.begin(), rnd.end(), generator);
+    
+    std::generate(rnd.begin(), rnd.end(), [&distribution, &engine]() { return distribution(engine); });
 
     // make it a valid version 4 (random) GUID
     // remember that on windows GUIDs are native endianness so this may need

de66e7a8e0ee143aebdcab05f623e6b582e99405
LADSoft <touchstone222@runbox.com>
2023-09-04 12:20:14 -0400

    undo bitint change

--- src/occparse/cpplookup.cpp
+++ src/occparse/cpplookup.cpp
@@ -4465,10 +4465,6 @@ void getSingleConversion(TYPE* tpp, TYPE* tpa, EXPRESSION* expa, int* n, e_cvsrn
                     {
                         seq[(*n)++] = CV_INTEGRALPROMOTION;
                     }
-                    else if (isbitint(tpp) || isbitint(tpa))
-                    {
-                        seq[(*n)++] = CV_INTEGRALPROMOTION;
-                    }
                     else if (isint(tpp))
                     {
                         // this next along with a change in the ranking takes care of the case where

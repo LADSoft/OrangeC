40b9e2961ecc843c99936f3b0f930f4268ffe08b
LADSoft <touchstone222@runbox.com>
2024-02-10 18:03:56 -0500

    update comment

--- src/occparse/init.cpp
+++ src/occparse/init.cpp
@@ -14,7 +14,7 @@
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
  * 
-
+ *     You should have received a copy of the GNU General Public License
  *     along with Orange C.  If not, see <http://www.gnu.org/licenses/>.
  * 
  *     contact information:

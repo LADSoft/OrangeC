e1b915724e32c92498a3bb31beab41305b29867e
LADSoft <touchstone222@runbox.com>
2025-04-12 03:19:43 -0400

    update copyright date

--- src/version.h
+++ src/version.h
@@ -4,5 +4,5 @@
 #define PRODUCT_STRING_VERSION STRING_VERSION
 #define PRODUCT_NAME "Orange C Compiler Package"
 #define CLIB_STRING_VERSION STRING_VERSION
-#define COPYRIGHT "Copyright (C) LADSoft 2006-2023"
-#define LIBRARY_COPYRIGHT "Copyright (C) LADSoft 1994-2023"
+#define COPYRIGHT "Copyright (C) LADSoft 2006-2025"
+#define LIBRARY_COPYRIGHT "Copyright (C) LADSoft 1994-2025"

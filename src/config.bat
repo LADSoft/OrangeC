22680f2c35e743e32ce000991f6c8d9aa77398e3
LADSoft <touchstone222@runbox.com>
2025-10-31 19:13:05 -0400

    update config.bat to use temp dirs in the occ tree

--- src/config.bat
+++ src/config.bat
@@ -2,4 +2,6 @@ set ORANGEC=\orangec
 set ORANGEC_HOME=orangec
 set LADSOFT_DEV=\cc386
 set PATH=\orangec\bin;\mingw64\bin;%PATH%;\program files\llvm\bin;\cc386\bin
+set TEMP=c:\orangec\libcxxtemp
+set TMP=c:\orangec\libcxxtemp
 call vcvars32.bat

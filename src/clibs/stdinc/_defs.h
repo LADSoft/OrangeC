fdd765704b5be22f5565722e9c77f8bb956a3cbd
LADSoft <touchstone222@runbox.com>
2026-03-14 16:07:20 -0400

    add __cpuid intrinsic (microsoft style)

--- src/clibs/stdinc/_defs.h
+++ src/clibs/stdinc/_defs.h
@@ -66,7 +66,8 @@
 #    endif
 
 #    if !defined(__STDC__) || defined(__STDC_VERSION__)
-#        define _RTL_INTRINS _RTL_FUNC _intrinsic
+#        define _RTL_INTRINS _RTL_FUNC _intrins
+#        define _RTL_FASTCALL __fastcall
 #    else
 #        define _RTL_INTRINS
 #    endif
